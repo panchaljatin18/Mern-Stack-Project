e6874606bfab4603807950a4189b2a0276cf7a18
Panchal Jatin <167307883+panchaljatin18@users.noreply.github.com>
2026-05-14 15:12:30 +0530

    up

--- backend/middleware/validate.js
+++ backend/middleware/validate.js
@@ -26,7 +26,7 @@ const validateRegister = [
     .normalizeEmail(),
   body("password")
     .notEmpty().withMessage("Password is required")
-    .isLength({ min: 6 }).withMessage("Password must be at least 6 characters")
+    .isLength({ min: 8 }).withMessage("Password must be at least 6 characters")
     .matches(/[A-Z]/).withMessage("Password must contain at least one uppercase letter")
     .matches(/[0-9]/).withMessage("Password must contain at least one number"),
   body("confirmPassword")
